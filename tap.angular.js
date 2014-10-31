2eafa4907b975ef7d36bb3c83dd1566e1c920a82
Ilya Pukhalski <ilya_pukhalski@epam.com>
2014-10-31 17:11:56 +0100

    Using tap with AngularJS

--- tap.angular.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-angular.module('ngTap', []).directive('ngTap', function() {
-    return function(scope, element, attrs) {
-        element.bind('tap', function() {
-            scope.$apply(attrs.ngTap, element);
-        });
-    };
-});
