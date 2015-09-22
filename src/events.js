e1c170613fc4a1224256999c8402df277fe3ac4d
Artem <a1tcoreoo@gmail.com>
2015-09-22 15:34:40 +0300

    Fix safari(ios9) property checking
    On safari(ios9) don't work tap event on not links element (span, div)

--- src/events.js
+++ src/events.js
@@ -1,6 +1,6 @@
     var eventMatrix = [{
         // Touchable devices
-        test: ('propertyIsEnumerable' in window || 'hasOwnProperty' in document) && (window.propertyIsEnumerable('ontouchstart') || document.hasOwnProperty('ontouchstart')),
+        test: ('propertyIsEnumerable' in window || 'hasOwnProperty' in document) && (window.propertyIsEnumerable('ontouchstart') || document.hasOwnProperty('ontouchstart') || window.hasOwnProperty('ontouchstart')),
         events: {
             start: 'touchstart',
             move: 'touchmove',
